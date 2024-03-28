43455b8807d2f9898303913509e3465d9c97f492
webgamedevelop <webgamedevelop@163.com>
2024-03-28 11:32:42 +0800

    merge the New function

--- logger_posix.go
+++ logger_posix.go
@@ -6,7 +6,6 @@ import (
 	"context"
 	"os"
 	"os/signal"
-	"slices"
 	"syscall"
 
 	"github.com/go-logr/logr"
@@ -14,42 +13,6 @@ import (
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
-
-// New return a new logr.Logger and its associated flush function.
-func New(ctx context.Context, cfg zapcore.EncoderConfig, writers ...zapcore.WriteSyncer) (logr.Logger, func()) {
-	f := defaultFile()
-	if f.Filename != "" {
-		writers = append(writers, f)
-	}
-	if stdout {
-		if !slices.ContainsFunc(writers, func(syncer zapcore.WriteSyncer) bool {
-			if syncer == os.Stdout {
-				return true
-			}
-			return false
-		}) {
-			writers = append(writers, os.Stdout)
-		}
-	}
-	if stderr {
-		if !slices.ContainsFunc(writers, func(syncer zapcore.WriteSyncer) bool {
-			if syncer == os.Stderr {
-				return true
-			}
-			return false
-		}) {
-			writers = append(writers, os.Stderr)
-		}
-	}
-
-	combinedWriteSyncer := zap.CombineWriteSyncers(writers...)
-	writer = combinedWriteSyncer
-	encoder := zapcore.NewConsoleEncoder(cfg)
-	if format == jsonLogFormat {
-		encoder = zapcore.NewJSONEncoder(cfg)
-	}
-	return newLogger(ctx, zapcore.Level(-enab), encoder, combinedWriteSyncer)
-}
 
 // NewLogger creates a new logr.Logger and its associated flush function.
 func newLogger(ctx context.Context, enabLevel zapcore.Level, encoder zapcore.Encoder, writeSyncer zapcore.WriteSyncer) (logr.Logger, func()) {
